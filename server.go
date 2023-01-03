4736a62514d0e6b7e37d8c331db066c11a97e8db
S.Wichai <wichai.s@wisesight.com>
2023-01-03 19:48:20 +0700

    feat: init server with graceful shutdown

--- server.go
+++ server.go
@@ -1,11 +1,40 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
-	fmt.Println("Please use server.go for main file")
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	go func() {
+		log.Println("Server is starting...")
+		if err := e.Start(os.Getenv("PORT")); err != nil && err != http.ErrServerClosed {
+			log.Fatal("shutting down server")
+		}
+	}()
+
 	fmt.Println("start at port:", os.Getenv("PORT"))
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt)
+	<-shutdown
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := e.Shutdown(ctx); err != nil {
+		log.Fatal(err)
+	}
 }

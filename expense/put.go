c98d2e95145f33fd4f28605bcb3fefa1016eefdf
S.Wichai <wichai.s@wisesight.com>
2023-01-05 21:21:36 +0700

    test: unit test update expense by id

--- expense/put.go
+++ expense/put.go
@@ -12,14 +12,14 @@ import (
 func (h *Handler) UpdateExpenseById(c echo.Context) error {
 	ex := new(Expense)
 	expenseId := c.Param("id")
-	query := `UPDATE expenses SET title = $1, amount = $2, note = $3, tags = $4 WHERE id = $5 RETURNING id`
 	err := c.Bind(&ex)
+	query := `UPDATE expenses SET title = $1, amount = $2, note = $3, tags = $4 WHERE id = $5 RETURNING id`
 
 	if err != nil {
 		return c.JSON(http.StatusBadRequest, Err{Message: "Request is invalid" + err.Error()})
 	}
 
-	row := h.db.QueryRow(query, ex.Title, ex.Amount, ex.Note, pq.Array(ex.Tags), expenseId)
+	row := h.db.QueryRow(query, ex.Title, ex.Amount, ex.Note, pq.Array(&ex.Tags), expenseId)
 	if err != nil {
 		log.Printf("Error updating expenses row id %s: %s", expenseId, err.Error())
 	}
@@ -30,7 +30,7 @@ func (h *Handler) UpdateExpenseById(c echo.Context) error {
 	case sql.ErrNoRows:
 		return c.JSON(http.StatusNotFound, Err{Message: "Updating target not found"})
 	case nil:
-		return c.JSON(http.StatusOK, ex)
+		return c.JSON(http.StatusNoContent, nil)
 	default:
 		return c.JSON(http.StatusInternalServerError, Err{Message: "Error updating expenses by id on DB: " + err.Error()})
 	}
